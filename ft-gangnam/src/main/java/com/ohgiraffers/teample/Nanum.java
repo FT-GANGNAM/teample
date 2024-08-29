247b32a42449acf499d595a459dc849bb410610b
temless <ljw980722@naver.com>
2024-08-29 12:48:38 +0900

    #2nanum

---
+++ ft-gangnam/src/main/java/com/ohgiraffers/teample/Nanum.java
@@ -0,0 +1,17 @@
+package main.java.com.ohgiraffers.teample;
+
+public class Nanum {
+
+    public int plus(int a, int b)
+    {
+        return a+b;
+    }
+
+    public int nanum(int a, int b){
+
+
+        return a/b;
+    }
+
+
+}
