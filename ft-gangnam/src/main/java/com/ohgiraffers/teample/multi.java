0879137503570e9475044d504bdd577a465ffe61
zxczx <zxczx3020@gmail.com>
2024-08-29 12:52:05 +0900

    multi

---
+++ ft-gangnam/src/main/java/com/ohgiraffers/teample/multi.java
@@ -0,0 +1,13 @@
+package main.java.com.ohgiraffers.teample;
+
+import java.util.Scanner;
+
+public class multi {
+
+    public static void multi(int a, int b){
+
+        int result = a*b;
+        System.out.println(" a * b = " + result);
+
+    }
+}
